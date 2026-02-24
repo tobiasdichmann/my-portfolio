1f9631de1c8b3b5f5938e1fae3479ca0a10580e6
Tobias <tobias.dichmanngmail.com>
2026-02-24 13:39:35 +0100

    Updated Skills
    Added GitHub and Antigravity.
    Changed Bootstrap and Tailwind CSS to be in the same box

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,45 +3,45 @@ import "../styles/about.scss";
 
 const About = () => {
   return (
-    <div id="about">
+    <div id='about'>
       <h3>Om Mig</h3>
 
-      <div className="divider"></div>
+      <div className='divider'></div>
 
       <p>Her kan du finde lidt information om, hvem jeg er, og hvad jeg kan</p>
 
-      <div className="about-content">
-        <div className="about-txt">
+      <div className='about-content'>
+        <div className='about-txt'>
           <h4>Hvem er jeg?</h4>
 
           <p>
-            Jeg er <b>uddannet webudvikler</b> med fokus på <b>frontend</b>.{" "}
+            Jeg er <b>uddannet webudvikler</b> med fokus på <b>frontend</b>.
             <br />
             Jeg koder hovedsageligt i <b>React.js</b> og med <b>SCSS</b> til
-            styling. I min fritid kan jeg godt lide at være sammen med mine{" "}
-            <b>venner</b>, spille <b>trommer</b> og ellers bare slappe af foran{" "}
+            styling. I min fritid kan jeg godt lide at være sammen med mine
+            <b>venner</b>, spille <b>trommer</b> og ellers bare slappe af foran
             <b>computeren</b>
           </p>
 
-          <a href="#contact">
+          <a href='#contact'>
             <button>Kontakt</button>
           </a>
         </div>
 
-        <div className="skills-content">
+        <div className='skills-content'>
           <h4>Jeg har erfaring med:</h4>
 
-          <div className="skills">
+          <div className='skills'>
             <div>HTML</div>
             <div>CSS / SCSS</div>
-            <div>Bootstrap</div>
-            <div>Tailwind CSS</div>
+            <div>Bootstrap / Tailwind CSS</div>
             <div>JavaScript</div>
             <div>React.js</div>
             <div>EmailJS</div>
+            <div>GitHub</div>
             <div>FileZilla</div>
             <div>TinyPNG</div>
-            <div>VS Code</div>
+            <div>VS Code / Antigravity</div>
             <div>Shopify</div>
           </div>
         </div>
